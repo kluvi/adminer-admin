957895ca650f13e99ed97e351766fcff834357aa
Jakub Kluvánek <kluvanek@gmail.com>
2017-07-16 22:01:07 +0200

    fix example

--- examples/index.php
+++ examples/index.php
@@ -1,19 +1,28 @@
 <?php
-require "./vendor/autoload.php";
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
 
-$downloadUrl = 'https://github.com/vrana/adminer/releases/download/v4.3.1/editor-4.3.1-mysql.php';
+require "../vendor/autoload.php";
+
+$downloadUrl = 'http://www.adminer.org/latest-editor.php';
 $targetDir = './adminer-download';
+$imagesBaseDir = './images/';
+$imagesBaseUrl = 'http://localhost/adminer-admin-test/images/';
+$db_database = 'test-adminer-admin';
+$db_host = 'localhost';
+$db_username = 'root';
+$db_password = '';
+
+$pluginsConfig = [
+    'field_image' => [
+        'baseDir' => $imagesBaseDir,
+        'baseUrl' => $imagesBaseUrl,
+    ],
+];
 
 
 // Uncomment this to download a copy of Adminer Editor from URL above an apply some patches to downloaded file
 //$downloader = new \kluvi\AdminerAdmin\Base\Downloader;
 //$downloader->download($downloadUrl, $targetDir);
 
-
-$pluginsConfig = [
-    'field_image' => [
-        'baseDir' => './images/',
-        'baseUrl' => 'http://localhost/adminer-admin-test/images/',
-    ],
-];
-\kluvi\AdminerAdmin\Base\AdminerFactory::run($targetDir . '/adminer.php', $database = 'test-adminer-admin', $host = 'localhost', $username = 'root', $password = '', $pluginsConfig);
+\kluvi\AdminerAdmin\Base\AdminerFactory::run($targetDir . '/adminer.php', $db_database, $db_host, $db_username, $db_password, $pluginsConfig);
