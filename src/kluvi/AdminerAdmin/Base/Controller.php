ac9eba2ee6c8243837935d03bc5409acd3796910
Jakub Kluvánek <kluvanek@gmail.com>
2018-04-28 18:54:59 +0200

    fix db name from config not env

--- src/kluvi/AdminerAdmin/Base/Controller.php
+++ src/kluvi/AdminerAdmin/Base/Controller.php
@@ -7,9 +7,16 @@ class Controller extends BaseController
 {
     public function index()
     {
+        if (config('database.default') !== 'mysql') {
+            throw new \InvalidArgumentException('Database driver must be "mysql');
+        }
         $adminerPath = config('adminer-admin.adminer_download_dir') . '/adminer.php';
         $pluginsConfig = config('adminer-admin.plugins_config');
+        $dbName = config('database.connections.mysql.database');
+        $dbHost = config('database.connections.mysql.host');
+        $dbUser = config('database.connections.mysql.username');
+        $dbPassword = config('database.connections.mysql.password');
 
-        AdminerFactory::run($adminerPath, env('DB_DATABASE'), env('DB_HOST'), env('DB_USERNAME'), env('DB_PASSWORD'), $pluginsConfig);
+        AdminerFactory::run($adminerPath, $dbName, $dbHost, $dbUser, $dbPassword, $pluginsConfig);
     }
 }
