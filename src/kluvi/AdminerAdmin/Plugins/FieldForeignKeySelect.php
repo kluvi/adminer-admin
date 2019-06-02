753c4adbf79871a922503bbc46bc1bbebed07b57
Jakub Kluvánek <kluvanek@gmail.com>
2019-06-02 20:40:40 +0200

    foreign keys in list table

--- src/kluvi/AdminerAdmin/Plugins/FieldForeignKeySelect.php
+++ src/kluvi/AdminerAdmin/Plugins/FieldForeignKeySelect.php
@@ -38,4 +38,44 @@ class FieldForeignKeySelect extends AbstractAdminPlugin
             }
         }
     }
+
+    function selectVal($val, $link, $field, $original)
+    {
+        // prints next column if exists as name
+        $fks = foreign_keys($_GET['select']);
+        foreach ($fks as $fk) {
+            if ($fk['source'][0] == $field['field']) {
+                $foreignFields = fields($fk['table']);
+                $foreignField = NULL;
+                $foreignNextField = NULL;
+                foreach ($foreignFields as $fkf) {
+                    if ($fkf['field'] == $fk['target'][0]) {
+                        $foreignField = $fkf;
+                        continue;
+                    }
+
+                    if ($foreignField !== NULL) {
+                        $foreignNextField = $fkf;
+                        break;
+                    }
+                }
+
+                $url = rawurldecode($link);
+                $parsedUrl = parse_url($url);
+                $parsedParams = NULL;
+                parse_str($parsedUrl['query'], $parsedParams);
+                $currentId = $parsedParams['where'][0]['val'] ?? NULL;
+                $currentIdColumn = $parsedParams['where'][0]['col'] ?? NULL;
+
+                if ($foreignNextField === NULL) {
+                    $query = "SELECT `{$foreignField['field']}`, `{$foreignField['field']}` AS asgasgasg FROM `{$fk['table']}` WHERE `{$currentIdColumn}`='{$currentId}' ORDER BY `{$foreignField['field']}`";
+                } else {
+                    $query = "SELECT `{$foreignField['field']}`, `{$foreignNextField['field']}` FROM `{$fk['table']}` WHERE `{$currentIdColumn}`='{$currentId}' ORDER BY `{$foreignField['field']}`";
+                }
+
+                $options = get_key_vals($query);
+                return '<a href="'.$link.'">'.reset($options).'</a>';
+            }
+        }
+    }
 }
